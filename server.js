524a326f5f0ada3b66d95c7677a2b72486b78e3e
idonia <sidoniag@gmail.com>
2020-08-07 15:40:30 -0500

    start build tables

---
+++ server.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+
+// create connecion to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3300,
+    // your MySQL username
+    user: 'root',
+    // your MySQL password
+    password: '',
+    database: 'employee_trackerDB'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId);
+    afterConnection();
+});
+
+afterConnection = () => {
+    connection.query('SELECT * FROM ', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+};
